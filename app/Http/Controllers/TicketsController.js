6d79fea8fe132ff8e8a3f4ef008cb0e9b8501933
Chimezie Enyinnaya <meziemichael@gmail.com>
2017-03-03 02:46:22 +0100

    open new support ticket

--- app/Http/Controllers/TicketsController.js
+++ app/Http/Controllers/TicketsController.js
@@ -1,7 +1,59 @@
 'use strict'
 
+const Validator = use('Validator')
+const Ticket = use('App/Model/Ticket')
+const RandomString = use('randomstring')
+const Category = use('App/Model/Category')
+
 class TicketsController {
 
+    /**
+     * Show the form for opening a new ticket.
+     */
+    * create (request, response) {
+        const categories = yield Category.pair('id', 'name')
+
+        yield response.sendView('tickets.create', {categories: categories})
+    }
+
+    /**
+     * Store a newly created ticket in database.
+     */
+    * store (request, response) {
+        // validate form input
+        const validation = yield Validator.validateAll(request.all(), {
+            title: 'required',
+            category: 'required',
+            priority: 'required',
+            message: 'required'
+        })
+
+        // show error messages upon validation fail
+        if (validation.fails()) {
+            yield request
+                .withAll()
+                .andWith({ errors: validation.messages() })
+                .flash()
+
+            return response.redirect('back')
+        }
+
+        // persist ticket to database
+        const ticket = yield Ticket.create({
+            title: request.input('title'),
+            user_id: request.currentUser.id,
+            ticket_id: RandomString.generate({ length: 10, capitalization: 'uppercase' }),
+            category_id: request.input('category'),
+            priority: request.input('priority'),
+            message: request.input('message'),
+            status: "Open",
+        })
+        // send mail notification
+        
+
+        yield request.with({ status: `A ticket with ID: #${ticket.ticket_id} has been opened.` }).flash()
+        response.redirect('back')
+    }
 }
 
 module.exports = TicketsController
--- app/Http/routes.js
+++ app/Http/routes.js
@@ -14,3 +14,11 @@ Route.post('register', 'AuthController.register')
 Route.get('login', 'AuthController.showLoginPage')
 Route.post('login', 'AuthController.login')
 Route.get('logout', 'AuthController.logout')
+
+/*
+|--------------------------------------------------------------------------
+| Tickets Routes
+|--------------------------------------------------------------------------
+*/
+Route.get('new_ticket', 'TicketsController.create');
+Route.post('new_ticket', 'TicketsController.store');
